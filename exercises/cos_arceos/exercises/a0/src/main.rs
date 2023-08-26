40f6e444dc32e4adc2b006dbda44e62c5d89a345
gosoline1 <3133047065@qq.com>
2023-08-26 18:26:34 +0800

    [your commit about experiment a0]

--- exercises/cos_arceos/exercises/a0/src/main.rs
+++ exercises/cos_arceos/exercises/a0/src/main.rs
@@ -11,6 +11,14 @@ use axstd::println;
 use axstd::println_prefix;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
+#[macro_export]
+macro_rules! println_prefix {
+    ($prefix:expr, $($arg:tt)*) => {
+        // 在输出前添加前缀并使用println!宏进行格式化输出
+        println!(concat!($prefix, "{}"), $($arg)*);
+    };
+}
+
 fn main() {
     println!("Hello, world!");
 
